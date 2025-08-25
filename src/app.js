17d73f4418462ad8458535931ff8fdc2de84ead6
amritesh-bhu <amriteshbhu438@gmail.com>
2025-08-25 12:32:11 +0530

    express inbuilt middleares for parsing and url!!

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import { HTTP_PORT } from "../.secrets/env.js"
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => {
     res.send("Hi there!!")
 })
 
 app.listen(HTTP_PORT, () => {
     console.log(`Server is listening on port ${HTTP_PORT}`)
-})
+})      
